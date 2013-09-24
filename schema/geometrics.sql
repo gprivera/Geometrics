2fe7c9bbbb7a3d2e9f1f0ffea51ff2a080a78114
gprivera <gprivera@live.com>
2013-09-24 23:24:09 +0800

    added initial database structure

---
+++ schema/geometrics.sql
@@ -0,0 +1,60 @@
+DROP DATABASE IF EXISTS `geometrics`;
+CREATE DATABASE `geometrics`;
+USE `geometrics`;
+
+DROP TABLE IF EXISTS `continents`;
+CREATE TABLE `continents`(
+	`id` int unsigned not null auto_increment primary key,
+	`name` varchar(45),
+	`code` varchar(2)
+);
+
+DROP TABLE IF EXISTS `countries`;
+CREATE TABLE `countries`(
+	`id` int unsigned not null auto_increment primary key,
+	`code_id` varchar(15),
+	`name` varchar(100),
+	`code` varchar(2),
+	`continent_id` int unsigned
+);
+
+DROP TABLE IF EXISTS `regions`;
+CREATE TABLE `regions`(
+	`id` int unsigned not null auto_increment primary key,
+	`code_id` varchar(15),
+	`code` varchar(10),
+	`local_code` varchar(10),
+	`name` varchar(100),
+	`continent_id` int unsigned,
+	`country_id` int unsigned
+);
+
+DROP TABLE IF EXISTS `airports`;
+CREATE TABLE `airports`(
+	`id` int unsigned not null auto_increment primary key,
+	`code_id` varchar(15),
+	`identification` varchar(10),
+	`type` varchar(45),
+	`name` varchar(100),
+	`latitude` varchar(15),
+	`longitude` varchar(15),
+	`elevation_feet` varchar(12),
+	`continent_id` int unsigned,
+	`country_id` int unsigned,
+	`region_id` int unsigned,
+	`municipality` varchar(45),
+	`gps_code` varchar(10),
+	`iata_code` varchar(5),
+	`local_code` varchar(5)
+	);
+
+DROP TABLE IF EXISTS `airport_frequencies`;
+CREATE TABLE `airport_frequencies`(
+	`id` int unsigned not null auto_increment primary key,
+	`code_id` varchar(15),
+	`airport_id` int unsigned,
+	`identification` varchar(10),
+	`type` varchar(45),
+	`description` varchar(45),
+	`frequency_mhz` varchar(15)
+);
